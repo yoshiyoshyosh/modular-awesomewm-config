6edc196b05f544bda4905aedcbe86d6a1d9eecd5
yosh <yosh-git@riseup.net>
2022-10-25 01:13:33 -0400

    workaround get_themes_dir

--- defs/theme.lua
+++ defs/theme.lua
@@ -4,7 +4,7 @@ local beautiful = require("beautiful")
 -- Themes define colours, icons, font and wallpapers.
 theme = "default"
 
-beautiful.init(gears.filesystem.get_themes_dir() .. theme .. "/theme.lua")
+beautiful.init(gears.filesystem.get_configuration_dir() .. "themes/" .. theme .. "/theme.lua")
 
 -- per-theme wallpaper and bar config
 require("themes." .. theme .. ".wallpaper")
